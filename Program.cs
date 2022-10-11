79e1fad642346cd6cca2eb644214676df46b66bd
timur <tupaev05@mail.ru>
2022-10-11 11:50:13 +0300

    Объединение пересечение разность массивов

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿int[] arr1 = new int[] { 1, 3, 5 };
+﻿/*int[] arr1 = new int[] { 1, 3, 5 };
 int[] arr2 = new int[] { 2, 4, 6, 8 };
 
 int len1 = arr1.Length;
@@ -47,3 +47,90 @@ for (int g = 0; g < res.Length; g++)
 {
     Console.WriteLine(res[g]);
 }
+*/
+/*
+using System;
+
+int[] arr1 = new int[] { 1, 3, 5,8,9 };
+int[] arr2 = new int[] { 1, 4, 6, 8 };
+
+int len1 = arr1.Length;
+int len2 = arr2.Length;
+int[] res = new int[0];
+
+int i = 0;
+int j = 0;
+int tmp = 0;
+while (i < len1 && j < len2)
+{
+    if (arr1[i] < arr2[j])
+    {
+        Array.Resize(ref res, res.Length + 1);
+        res[tmp] = arr1[i];
+        tmp += 1;
+        i+= 1;
+    }
+    else if(arr1[i] > arr2[j])
+    {
+        j += 1;
+    }
+    else
+    {
+        i += 1;
+    }
+}
+if (i != len1)
+{
+    for (int k = i; k < len1; k++)
+    {
+        Array.Resize(ref res, res.Length + 1);
+        res[tmp] = arr1[k];
+        tmp += 1;
+    }
+
+
+}
+
+for (int g = 0; g < res.Length; g++)
+{
+    Console.WriteLine(res[g]);
+
+}
+*/
+/*int[] arr1 = new int[] { 1, 3, 5, 8, 9 };
+int[] arr2 = new int[] { 1, 4, 6, 8 };
+
+int len1 = arr1.Length;
+int len2 = arr2.Length;
+int[] res = new int[0];
+
+
+int i = 0;
+int j = 0;
+int tmp = 0;
+while (i < len1 && j < len2)
+{
+    if (arr1[i] < arr2[j])
+    {
+        i += 1;
+    }
+    else if (arr1[i] > arr2[j])
+    {
+        j += 1;
+    }
+    else
+    {
+        Array.Resize(ref res, res.Length + 1);
+        res[tmp] = arr1[i];
+        tmp += 1;
+        i += 1;
+        j += 1;
+    }
+}
+
+for (int g = 0; g < res.Length; g++)
+{
+    Console.WriteLine(res[g]);
+
+}
+*/
