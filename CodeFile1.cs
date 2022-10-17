9f3db0ed56eaabc6cf73ee1fc80dcbf6674a5840
timur <tupaev05@mail.ru>
2022-10-17 15:18:35 +0300

    задача 1 вар

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -1,206 +1,47 @@
-﻿using System.Transactions;
-
-int n = 4;
-int[,] matr = new int[4,4]
+﻿
+using System.Diagnostics.CodeAnalysis;
+Random random = new Random();
+int len = random.Next(10);
+int[] b = new int[len] ;
+for(int i = 0; i < len; i++)
 {
-    { 1,2,3,4},
-    { 0,0,0,5},
-    { 0,0,0,6},
-    { 7,0,0,7}
-};
-//задача 1 еденичная матрица
-/*for(int i = 0; i < n; i++)
+    b[i] = random.Next(50);
+}
+int max = b[0];
+int min = b[0];
+int maxpos = 0;
+int minpos = 0;
+for(int i = 0; i < b.Length; i++)
 {
-    for(int j = 0; j < n; j++)
+    if (b[i] > max)
     {
-        if (i == j)
-        {
-            matr[i, j] = 1;
-        }
-        else
-        {
-            matr[i, j] = 0;
-        }
+        max = b[i];
+        maxpos = i;
+    }
+    if (b[i] < min)
+    {
+        min = b[i];
+        minpos = i;
     }
 }
-*/
-//Задача 2 ыерхнетреугольная
-/*
-for (int i = 0; i < n; i++)
-{
-    
-    for (int j = 0; j < n; j++)
-    {
-        if (j <= i)
-        {
-            matr[i, j] = j+1;
-        }
-        else { matr[i, j] = 0; }
-
-    }
-}
-*/
-//Задача 3 нижнетреугольная
-/*
-for (int i = 0; i < n; i++)
-{
-    int tmp = 1;
-    for (int j = 0; j < n; j++)
-    {
-        if (j >= i)
-        {
-            matr[i, j] = tmp;
-            tmp += 1;
-        }
-        else { matr[i, j] = 0; }
-
-    }
-}
-*/
-//Задача 4 сумма граней
-/*int sum = 0;
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        if (i == 0||j==0||j==n-1||i==n-1)
-        {
-            sum += matr[i, j];
-        }
-    }
-}
-*/
-//Задача 5 сумма крест 
-/*int sum = 0;
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        if ((i==j)||(i+j==n-1))
-        { 
-            sum += matr[i, j]; 
-        }
-    }
-}
-*/
-// Задаса 6 ромб
-/*
 int sum = 0;
-int start = 0;
-int end = n;
-int stMn = 1;
-int endMn = 1;
-for (int i = 0; i < n; i++)
+if (maxpos > minpos)
 {
-    if (Math.Abs(start - end)==1)
+    for (int i = minpos+1; i < maxpos; i++)
     {
-        stMn = -1;
-        endMn = -1;
-    }
-    
-
-    for (int j =start; j < end; j++)
-    {
-            sum += matr[i, j];
-    }
-    start += stMn;
-    end -= endMn;
-}
-*/
-//Повернуть матрицу
-
-
-int length = n;
-for (int i = 0; i < length / 2; i++)
-{
-    int first = i;
-    int last = length - 1 - i;
-    for (int j = i; j < last; j++)
-    {
-        int tmp = j - first;
-        int top = matr[first,j];
-        matr[first,j] = matr[last - tmp,first];
-        matr[last - tmp,first] = matr[last,last - tmp];
-        matr[last,last - tmp] = matr[j,last];
-        matr[j,last] = top;
+        sum += b[i];
     }
 }
-    //проверка на еденичную матрицу
-    /*bool res = true;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j && matr[i,j]!=1)
-            {
-                res = false;
-            }
-            else if (i != j && matr[i, j] != 0)
-            {
-                res = false;
-            }
-        }
-    }
-    if (res)
-    {
-        Console.WriteLine("yes");
-    }
-    else
-    {
-        Console.WriteLine("no");
-    }
-    */
-    //проверка на симмерричность
-    /*bool res = true;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (matr[i, j] != matr[n-i-1,n-j-1])
-            {
-                res = false;
-            }
-        }
-    }
-    if (res)
-    {
-        Console.WriteLine("yes");
-    }
-    else
-    {
-        Console.WriteLine("no");
-    }
-    */
-    // проверка на верзнетреугольность 
-    /*bool res = true;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i > j && matr[i,j]!=0)
-            {
-                res = false;
-            }
-        }
-    }
-    if (res)
-    {
-        Console.WriteLine("yes");
-    }
-    else
-    {
-        Console.WriteLine("no");
-    }
-    */
-
-    for (int i = 0; i < n; i++)
+else
 {
-    for (int j = 0; j < n; j++)
+    for (int i = maxpos + 1; i < minpos; i++)
     {
-        Console.Write($"{matr[i, j],4}");
+        sum += b[i];
     }
-    Console.WriteLine();
 }
-
-
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine(b[i]);
+}
+Console.WriteLine("res {0}",sum);
 
--- Program.cs
+++ Program.cs
@@ -136,3 +136,278 @@ for (int g = 0; g < res.Length; g++)
 
 }
 */
+/*using System.Transactions;
+
+int n = 4;
+int[,] matr = new int[4,4]
+{
+    { 1,2,3,4},
+    { 0,0,0,5},
+    { 0,0,0,6},
+    { 7,0,0,7}
+};
+*/
+//задача 1 еденичная матрица
+/*for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        if (i == j)
+        {
+            matr[i, j] = 1;
+        }
+        else
+        {
+            matr[i, j] = 0;
+        }
+    }
+}
+*/
+//Задача 2 ыерхнетреугольная
+/*
+for (int i = 0; i < n; i++)
+{
+    
+    for (int j = 0; j < n; j++)
+    {
+        if (j <= i)
+        {
+            matr[i, j] = j+1;
+        }
+        else { matr[i, j] = 0; }
+
+    }
+}
+*/
+//Задача 3 нижнетреугольная
+/*
+for (int i = 0; i < n; i++)
+{
+    int tmp = 1;
+    for (int j = 0; j < n; j++)
+    {
+        if (j >= i)
+        {
+            matr[i, j] = tmp;
+            tmp += 1;
+        }
+        else { matr[i, j] = 0; }
+
+    }
+}
+*/
+//Задача 4 сумма граней
+/*int sum = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == 0||j==0||j==n-1||i==n-1)
+        {
+            sum += matr[i, j];
+        }
+    }
+}
+*/
+//Задача 5 сумма крест 
+/*int sum = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if ((i==j)||(i+j==n-1))
+        { 
+            sum += matr[i, j]; 
+        }
+    }
+}
+*/
+// Задаса 6 ромб
+/*
+int sum = 0;
+int start = 0;
+int end = n;
+int stMn = 1;
+int endMn = 1;
+for (int i = 0; i < n; i++)
+{
+    if (Math.Abs(start - end)==1)
+    {
+        stMn = -1;
+        endMn = -1;
+    }
+    
+
+    for (int j =start; j < end; j++)
+    {
+            sum += matr[i, j];
+    }
+    start += stMn;
+    end -= endMn;
+}
+*/
+//Повернуть матрицу
+
+
+/*int length = n;
+for (int i = 0; i < length / 2; i++)
+{
+    int first = i;
+    int last = length - 1 - i;
+    for (int j = i; j < last; j++)
+    {
+        int tmp = j - first;
+        int top = matr[first,j];
+        matr[first,j] = matr[last - tmp,first];
+        matr[last - tmp,first] = matr[last,last - tmp];
+        matr[last,last - tmp] = matr[j,last];
+        matr[j,last] = top;
+    }
+}
+*/
+//проверка на еденичную матрицу
+/*bool res = true;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == j && matr[i,j]!=1)
+        {
+            res = false;
+        }
+        else if (i != j && matr[i, j] != 0)
+        {
+            res = false;
+        }
+    }
+}
+if (res)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+*/
+//проверка на симмерричность
+/*bool res = true;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (matr[i, j] != matr[n-i-1,n-j-1])
+        {
+            res = false;
+        }
+    }
+}
+if (res)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+*/
+// проверка на верзнетреугольность 
+/*bool res = true;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i > j && matr[i,j]!=0)
+        {
+            res = false;
+        }
+    }
+}
+if (res)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+*/
+/*
+for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < n; j++)
+{
+    Console.Write($"{matr[i, j],4}");
+}
+Console.WriteLine();
+}
+
+*/
+/*
+ static void Umn(int[,] matr)
+{
+    int[,] res = new int[matr.GetLength(0), matr.GetLength(1)];
+
+    
+    for (int i = 0; i < matr.GetLength(0); i++)
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            res[i, j] = 0;
+            for (int k = 0; k < matr.GetLength(1); k++)
+                res[i, j] += matr[i, k] * matr[k, j];
+        }
+    for (int i = 0; i < res.GetLength(0); i++)
+    {
+        for (int j = 0; j < res.GetLength(1); j++)
+        {
+            Console.Write($"{res[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+/*
+static void swap(int[,] matr)
+{
+    
+    for (int i = 0; i < matr.GetLength(0); i=i+2)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i< matr.GetLength(0) - 1) {
+                var tmp = matr[i, j];
+                matr[i, j] = matr[i+1 , j];
+                matr[i + 1, j] = tmp;
+            }
+
+        }
+    }
+
+
+
+
+    for (int i = 0; i< matr.GetLength(0); i++)
+    {
+        for (int j = 0; j <matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+/*
+int[,] m1 = new int[5, 2]
+{
+            { 1,1 },
+            {1,2 },
+    {1,3 },
+    {1,4 },
+    {1,5 }
+};
+int[,] m2 = new int[2, 2]
+{
+            { 1,1 },
+            {1,1 }
+};
+*/
