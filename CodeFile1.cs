12ec64a281ed5dbfc5939dfba7bf0b98ca5bf94e
timur <tupaev05@mail.ru>
2022-10-18 14:25:59 +0300

    List

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -1,47 +1,141 @@
-﻿
-using System.Diagnostics.CodeAnalysis;
-Random random = new Random();
-int len = random.Next(10);
-int[] b = new int[len] ;
-for(int i = 0; i < len; i++)
+﻿using System;
+using System.Runtime.CompilerServices;
+/*
+static void Umn(int[,] matr, ref int[,] matrold,int kol)
 {
-    b[i] = random.Next(50);
-}
-int max = b[0];
-int min = b[0];
-int maxpos = 0;
-int minpos = 0;
-for(int i = 0; i < b.Length; i++)
+for (int s = 1; s < kol; s++)
 {
-    if (b[i] > max)
-    {
-        max = b[i];
-        maxpos = i;
-    }
-    if (b[i] < min)
-    {
-        min = b[i];
-        minpos = i;
-    }
-}
-int sum = 0;
-if (maxpos > minpos)
+int[,] res = new int[matr.GetLength(0), matr.GetLength(1)];
+for (int i = 0; i < matr.GetLength(0); i++)
+for (int j = 0; j < matr.GetLength(1); j++)
 {
-    for (int i = minpos+1; i < maxpos; i++)
-    {
-        sum += b[i];
-    }
+res[i, j] = 0;
+for (int k = 0; k < matr.GetLength(1); k++)
+res[i, j] += matr[i, k] * matrold[k, j];
 }
-else
-{
-    for (int i = maxpos + 1; i < minpos; i++)
-    {
-        sum += b[i];
-    }
+matrold = res;
 }
-for (int i = 0; i < len; i++)
-{
-    Console.WriteLine(b[i]);
-}
-Console.WriteLine("res {0}",sum);
 
+
+}
+int[,] m2 = new int[2,2]
+{
+{1,1},
+{1,0}
+
+
+};
+Umn(m2,ref m2,6);
+for (int i = 0; i <m2.GetLength(0); i++)
+{
+for (int j = 0; j < m2.GetLength(1); j++)
+{
+Console.Write($"{m2[i, j],4}");
+}
+Console.WriteLine();
+}
+*/
+/*
+List<int> list1 = new List<int>();
+var kol = 5;
+for(int i = 0; i < kol; i++)
+{
+    list1.Add(i);
+}
+string s = "hello";
+foreach(object k in s)
+{
+    Console.WriteLine(k);
+}
+list1.Remove(0);
+for (int i = 0; i < list1.Count; i++)
+{
+    Console.WriteLine(list1[i]);
+}
+Console.WriteLine("{0}",list1.Contains(0));
+
+*/
+
+
+string s1 = "потоп информатика aбв шалаш";
+var t = s1.Split(" ");
+List<string> pol = new List<string>();
+List<string> povtor = new List<string>();
+List<string> alfa = new List<string>();
+List<string> len = new List<string>();
+string smin = t[0];
+
+string smax = t[0];
+foreach (string word in t)
+{
+    if (word.Length >= smin.Length)
+    {
+        smin = word;
+    }
+    if (word.Length <= smax.Length)
+    {
+        smax = word;
+    }
+    bool alf = true;
+    for (int i = 1; i < word.Length; i++)
+    {
+
+        if (word[i] < word[i - 1])
+        {
+            alf = false;
+        }
+
+    }
+    if (alf)
+    {
+        alfa.Add(word);
+    }
+    bool polindrom = true;
+    
+    int count = 0;
+    char first = word[0];
+    for (int i=0; i <word.Length; i++)
+    {
+        
+        if (word[i] != word[word.Length  - i-1])
+        {
+            polindrom = false;
+            
+        }
+        if (word[i] == first)
+        {
+            count++;
+        }
+    }
+    if (polindrom)
+    {
+        pol.Add(word);
+    }
+    if (count>1)
+    {
+        povtor.Add(word);
+    }
+}
+
+
+len.Add(smax);
+len.Add(smin);
+foreach (object k in pol)
+{
+    Console.WriteLine(k);
+}
+Console.WriteLine();
+foreach (object k in povtor)
+{
+    Console.WriteLine(k);
+}
+Console.WriteLine();
+foreach (object k in alfa)
+{
+    Console.WriteLine(k);
+}
+Console.WriteLine();
+foreach (object k in len)
+{
+    Console.WriteLine(k);
+}
